7ca9fe37b280ef096541d18000abf9d0c17b83ef
AndreevIlya <katauri@yandex.ru>
2019-07-09 23:19:08 +0300

    Two threads done.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
-    public static void main(String[] args) {
+    private static final int COUNT = 5;
+    private volatile char currentLetter = 'A';
 
+    public static void main(String[] args) {
+        Main m = new Main();
+        new Thread(m::printA).start();
+        new Thread(m::printB).start();
     }
+
+    private synchronized void printA() {
+        for (int i = 0; i < COUNT; i++) {
+            try {
+                while (currentLetter != 'A') {
+                    this.wait();
+                }
+                System.out.print(currentLetter);
+                currentLetter = 'B';
+                this.notify();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private synchronized void printB() {
+        for (int i = 0; i < COUNT; i++) {
+            try {
+                while (currentLetter != 'B') {
+                    this.wait();
+                }
+                System.out.print(currentLetter);
+                currentLetter = 'A';
+                this.notify();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
