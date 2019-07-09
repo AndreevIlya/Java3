47e85114e321fc3261cc44751ad9bdc308263424
AndreevIlya <katauri@yandex.ru>
2019-07-09 23:53:31 +0300

    Three threads done.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
-    private static final int COUNT = 5;
+    private static final int COUNT = 10;
     private volatile char currentLetter = 'A';
 
     public static void main(String[] args) {
         Main m = new Main();
         new Thread(m::printA).start();
         new Thread(m::printB).start();
+        new Thread(m::printC).start();
     }
 
     private synchronized void printA() {
@@ -15,8 +16,9 @@ public class Main {
                     this.wait();
                 }
                 System.out.print(currentLetter);
-                currentLetter = 'B';
-                this.notify();
+                if (currentLetter == 'A') currentLetter = 'B';
+                else currentLetter = 'C';
+                this.notifyAll();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -30,13 +32,28 @@ public class Main {
                     this.wait();
                 }
                 System.out.print(currentLetter);
-                currentLetter = 'A';
-                this.notify();
+                if (currentLetter == 'B') currentLetter = 'C';
+                else currentLetter = 'A';
+                this.notifyAll();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
 
-
+    private synchronized void printC() {
+        for (int i = 0; i < COUNT; i++) {
+            try {
+                while (currentLetter != 'C') {
+                    this.wait();
+                }
+                System.out.print(currentLetter);
+                if (currentLetter == 'C') currentLetter = 'A';
+                else currentLetter = 'B';
+                this.notifyAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
