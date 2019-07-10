4a497cafdff54e9237d335c5ff1101124bbbfd94
AndreevIlya <katauri@yandex.ru>
2019-07-10 11:58:53 +0300

    Three print methods remade into one.

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,22 @@
 public class Main {
-    private static final int COUNT = 100;
+    private static final int COUNT = 10;
     private volatile char currentLetter = 'A';
 
     public static void main(String[] args) {
         Main m = new Main();
-        new Thread(m::printA).start();
-        new Thread(m::printB).start();
-        new Thread(m::printC).start();
+        new Thread(() -> m.printLetter('A', 'B')).start();
+        new Thread(() -> m.printLetter('B', 'C')).start();
+        new Thread(() -> m.printLetter('C', 'A')).start();
     }
 
-    private synchronized void printA() {
+    private synchronized void printLetter(char first, char next) {
         for (int i = 0; i < COUNT; i++) {
             try {
-                while (currentLetter != 'A') {
+                while (currentLetter != first) {
                     this.wait();
                 }
                 System.out.print(currentLetter);
-                currentLetter = 'B';
-                this.notifyAll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private synchronized void printB() {
-        for (int i = 0; i < COUNT; i++) {
-            try {
-                while (currentLetter != 'B') {
-                    this.wait();
-                }
-                System.out.print(currentLetter);
-                currentLetter = 'C';
-                this.notifyAll();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private synchronized void printC() {
-        for (int i = 0; i < COUNT; i++) {
-            try {
-                while (currentLetter != 'C') {
-                    this.wait();
-                }
-                System.out.print(currentLetter);
-                currentLetter = 'A';
+                currentLetter = next;
                 this.notifyAll();
             } catch (InterruptedException e) {
                 e.printStackTrace();
