7ea6c0067f5e7f13b9675e477f648c4c40b82874
AndreevIlya <katauri@yandex.ru>
2019-07-18 14:09:21 +0300

    Second array test task done.

--- src/ArrayWith4.java
+++ src/ArrayWith4.java
@@ -5,6 +5,7 @@ public class ArrayWith4 {
     private static final int[] array = {2, 4, 6, 8, 9, 1, 3, 5, 7, 9, 0, 4, 5, 6, 4, 2, 7, 4, 3, 6, 2, 8, 9};
 
     public static void main(String[] args) {
+        System.out.println(check1And4Presence(array));
         writeArray(array);
         writeArray(getItemsAfterLast4(array));
 
@@ -32,5 +33,16 @@ public class ArrayWith4 {
         int[] result = new int[arr.length - position - 1];
         System.arraycopy(arr, position + 1, result, 0, arr.length - position - 1);
         return result;
+    }
+
+    @Contract(pure = true)
+    static boolean check1And4Presence(@NotNull int[] arr) {
+        boolean check1 = false, check4 = false;
+        for (int a : arr) {
+            if (a == 1) check1 = true;
+            if (a == 4) check4 = true;
+            if (check1 && check4) return true;
+        }
+        return false;
     }
 }
